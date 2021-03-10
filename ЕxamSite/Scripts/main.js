bf16c785707fcfcb6c0e2c234e30ea59ddbe668b
Grosta <denis.axverdovlk@gmail.com>
2021-03-10 22:13:37 +0200

    Squalid site

---
+++ ЕxamSite/Scripts/main.js
@@ -0,0 +1,14 @@
+var res = document.getElementById('Res');
+var vol = document.getElementById('Vol');
+var cur = document.getElementById('Cur');
+var butt = document.getElementById('Butt');
+
+
+function click(){
+	if (butt.clicked) {
+		alert(2);
+	}
+	else{
+		alert(1);
+	}
+}
